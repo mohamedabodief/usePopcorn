275c280feb04702d336a82305551a77e1ee31bfe
Mohamed Abodief <mohamed.abodeaf@gmail.com>
2025-10-12 02:09:27 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import StarRating from './StarRating';
 // import './index.css';
 // import App from './App';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={5} onSetRating={setMovieRating} />
+      <p>This movie has a rating of {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
-    <StarRating maxRating={3} />
-    <StarRating />
+    <StarRating maxRating={5} defaultRating={2} color="blue" size={48} messages={['terrible', 'bad', 'okay', 'good', 'great']} />
+    <Test />
   </React.StrictMode>
 );
